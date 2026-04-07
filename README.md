4bd917ae4c4992dfcd4d0a573535c43e2a452844
Lyndon Domini Catan <lyndoncatan75@gmail.com>
2026-04-07 03:24:45 +0000

    Add sheep1, sheep2, and cow2 instances to demonstrate prototype pattern

--- README.md
+++ README.md
@@ -20,10 +20,19 @@ java -cp out prototype.TestAnimal
 ## Example output
 
 ```text
-Sheep{name='Molly', legs=4, sound='Baa', food='Grass'}
-Molly the sheep says: Baa
+=== Sheep Prototypes ===
+Sheep{name='Wool', legs=4, sound='Baa', food='Grass'}
+Wool the sheep says: Baa
+Sheep{name='Fluffy', legs=4, sound='Baa', food='Grass'}
+Fluffy the sheep says: Baa
+
+=== Cow Prototypes ===
 Cow{name='Bessie', legs=4, sound='Moo', food='Hay'}
 Bessie the cow says: Moo
+Cow{name='Daisy', legs=4, sound='Moo', food='Hay'}
+Daisy the cow says: Moo
+
+=== Horse Prototype ===
 Horse{name='Spirit', color='Black', legs=4, sound='Neigh', food='Oats'}
 Spirit the horse says: Neigh
 ```
--- src/prototype/TestAnimal.java
+++ src/prototype/TestAnimal.java
@@ -4,16 +4,32 @@ public class TestAnimal {
     public static void main(String[] args) {
         AnimalRegistry registry = new AnimalRegistry();
 
-        Animal sheep = registry.createSheep("Molly");
-        Animal cow = registry.createCow("Bessie");
+        // Create sheep instances
+        Animal sheep1 = registry.createSheep("Wool");
+        Animal sheep2 = registry.createSheep("Fluffy");
+        
+        // Create cow instances
+        Animal cow1 = registry.createCow("Bessie");
+        Animal cow2 = registry.createCow("Daisy");
+        
+        // Create horse instance
         Animal horse = registry.createHorse("Spirit", "Black");
 
-        System.out.println(sheep);
-        sheep.makeSound();
+        System.out.println("=== Sheep Prototypes ===");
+        System.out.println(sheep1);
+        sheep1.makeSound();
+        
+        System.out.println(sheep2);
+        sheep2.makeSound();
 
-        System.out.println(cow);
-        cow.makeSound();
+        System.out.println("\n=== Cow Prototypes ===");
+        System.out.println(cow1);
+        cow1.makeSound();
+        
+        System.out.println(cow2);
+        cow2.makeSound();
 
+        System.out.println("\n=== Horse Prototype ===");
         System.out.println(horse);
         horse.makeSound();
     }
